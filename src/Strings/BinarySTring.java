20232f7ed76c3ab5e58006d5ef9de88e8774f9a4
Abhigyan Raha <abhigyanraha76@gmail.com>
2021-02-02 23:46:21 +0530

    02-02-2020

--- src/Strings/BinarySTring.java
+++ src/Strings/BinarySTring.java
@@ -1,5 +1,26 @@
 package Strings;
 
+/**
+ * Given a binary string S. The task is to count the number of substrings that start and end with 1. For example, if the input string is “00100101”, then there are three substrings “1001”, “100101” and “101”.
+ *
+ * Example 1:
+ *
+ * Input:
+ * N = 4
+ * S = 1111
+ * Output: 6
+ * Explanation: There are 6 substrings from
+ * the given string. They are 11, 11, 11,
+ * 111, 111, 1111.
+ * Example 2:
+ *
+ * Input:
+ * N = 5
+ * S = 01101
+ * Output: 3
+ * Explanation: There 3 substrings from the
+ * given string. They are 11, 101, 1101.
+ */
 public class BinarySTring {
     public static void main(String[] args) {
         System.out.println(binarySubstring("010101"));
